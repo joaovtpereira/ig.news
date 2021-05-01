41f5712103dca4399f4eb7cd49d3ceee5228c9f7
joaovtpereira <joaovitorpessoal2@gmail.com>
2021-05-01 16:23:19 -0300

    new: Add de comunicação com a api de pagamento do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import {GetServerSideProps} from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product : {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product} : HomeProps) {
   return (
     <>
       <Head>
@@ -17,9 +26,9 @@ export default function Home() {
 
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 mounth</span>
+            <span>for {product.amount} mounth</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding"/>
@@ -27,3 +36,20 @@ export default function Home() {
     </>
   )
 }
+
+export const getServerSideProps : GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1ImNqyDHdz3os8H2TQjWMstI');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+  return {
+    props: {
+      product,
+    }
+  }
+} 
